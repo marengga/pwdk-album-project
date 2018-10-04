a5816e108a294c0c05356c1d60e3739641e59259
Andri K. Marengga <andry.marengga@visionet.co.id>
2018-10-04 14:57:18 +0700

    Fix UI problem: ScrollView can't be scrolled to bottom

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View>
+      <View style={{flex:1}}>
         <Header headerText='My Albums' />
 
         <AlbumList />
